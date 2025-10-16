e5a93793039abb6bbed7e52c50db32d2ba2000f1
Alex <udinkik@mail.ru>
2025-10-16 13:06:06 +0300

    KS-1-2-second-task

---
+++ src/main/kotlin/lesson_1/lesson_1_2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+    val countOfOrders = 75
+    val congratText = "Thx for buy!"
+
+    println(countOfOrders)
+    println(congratText)
+
+    var countOfEmpl = 2000
+    //println(countOfEmpl)
+
+    countOfEmpl-= 1
+    println(countOfEmpl)
+}
