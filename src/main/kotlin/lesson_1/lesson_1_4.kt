8a8c663b1ca19c4d82678753b0522d8734a22399
Alex <udinkik@mail.ru>
2025-10-21 22:21:00 +0300

    KS-1-4-five-task-ref

--- src/main/kotlin/lesson_1/lesson_1_4.kt
+++ src/main/kotlin/lesson_1/lesson_1_4.kt
@@ -1,10 +1,10 @@
 package lesson_1
 
 fun main() {
-    val length:Long = 40_868_600_000
-    val age:Byte = 27
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
     val partOfDay: Float = 0.075f
-    val seconds:Short = 6480
+    val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
+    val apogee: Int = 327000
 }
